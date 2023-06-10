564cd45c8f9990c5ff5b86b5ac08f13dc487ee6c
gaurav876409 <gaurav876409@gmail.com>
2023-06-10 11:51:03 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [counter, setCounter] = useState(1);
 
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    if(counter>1){
+      setCounter(counter - 1)
+    }
+  }
+
+  const counterClass = () => {
+    if(counter%3 && counter%5 === 0){
+      return 'fizzbuzz' 
+    }else if(counter%3 === 0){
+      return 'fizz'
+    }else if(counter%5 === 0){
+      return 'buzz'
+    }else{
+      return 'normal'
+    }
+  }
   
   return (
     <div id="main">
-
+      <div id='counter' className={counterClass()}>{counter}</div>
+      <div>
+        <button id='increment' onClick={increment}>Increment</button>
+      </div>
+      <div>
+        <button id='decrement' onClick={decrement}>Decrement</button>
+      </div>
     </div>
   )
 }
